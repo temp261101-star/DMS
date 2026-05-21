42a452ead91fc7ce848c691d74591735c4c96c15
sayalip13 <sayalipatil4162@gmail.com>
2026-05-21 12:42:43 +0530

    change dms 1

--- create_tables.sql
+++ create_tables.sql
@@ -7,7 +7,6 @@ CREATE TABLE customers (
     customer_id   INT PRIMARY KEY AUTO_INCREMENT,
     last_name     VARCHAR(50)  NOT NULL,
     email         VARCHAR(100) NOT NULL UNIQUE,
-    phone         VARCHAR(20),
     created_at    TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
 );
 
