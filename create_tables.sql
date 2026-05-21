6dbc525f92818458518404134b796d6ae48d2e39
sayalip13 <sayalipatil4162@gmail.com>
2026-05-21 13:02:03 +0530

    change dms 43

--- create_tables.sql
+++ create_tables.sql
@@ -6,7 +6,6 @@ USE ecommerce;
 CREATE TABLE customers (
     customer_id   INT PRIMARY KEY AUTO_INCREMENT,
     name     VARCHAR(50)  NOT NULL,
-    email         VARCHAR(100) NOT NULL UNIQUE,
     created_at    TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
 );
 
