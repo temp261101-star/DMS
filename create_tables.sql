df521a9cf1d02048c51b640b8719c4d14398ac64
sayalip13 <sayalipatil4162@gmail.com>
2026-05-21 14:39:19 +0530

    change dms 40

--- create_tables.sql
+++ create_tables.sql
@@ -13,7 +13,6 @@ CREATE TABLE products (
     product_id    INT PRIMARY KEY AUTO_INCREMENT,
     name          VARCHAR(100) NOT NULL,
     description   TEXT,
-    price         DECIMAL(10,2) NOT NULL,
     category      VARCHAR(50),
     created_at    TIMESTAMP     DEFAULT CURRENT_TIMESTAMP
 );
