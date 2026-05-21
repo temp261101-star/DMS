1da6b33a11f70491dda34dc870825d936b965d1a
sayalip13 <sayalipatil4162@gmail.com>
2026-05-21 15:39:08 +0530

    change 115

--- create_tables.sql
+++ create_tables.sql
@@ -2,6 +2,6 @@ CREATE TABLE employee (
     employee_id   INT PRIMARY KEY AUTO_INCREMENT,
     first_name    VARCHAR(50) NOT NULL,
     email         VARCHAR(100) UNIQUE,
-    phone_number  VARCHAR(15),
+    phone_number  INT(15),
     joining_date  DATE
 );
